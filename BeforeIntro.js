7e384cb7a20235a944ccafb1b08ffa85142e837b
NikolasPapas25 <77715367+NikolasPapas25@users.noreply.github.com>
2021-03-05 13:30:38 +0200

    Work In Prog

---
+++ BeforeIntro.js
@@ -0,0 +1,15 @@
+
+function signup(){
+const user = document.getElementById('user')
+const pass = document.getElementById('pass')
+const username = user.value
+const password = pass.value
+localStorage.setItem('username', username)
+localStorage.setItem('pass', password)
+const btn = document.getElementById('btn')
+window.open('/intro.html')
+window.setTimeout(closew, 1000)
+}
+function closew(){
+    window.close()
+}
--- Feedback.js
+++ Feedback.js
@@ -1,24 +1,14 @@
 const user_input = document.getElementById('Pass');
-const Enter_Button = document.getElementById('Enter');
 const conti = document.getElementById('conti');
 const client_Pass = user_input.value;
 const em = document.getElementById('EmInput');
 const Clients = []
-function Get_Data(){
-    if(user_input.value == null){
-        window.alert('Please Enter a Password');
-    }else{
-        window.alert(user_input.value);
-        localStorage.setItem('clientPass', user_input.value);
-        Enter_Button.remove();
-        Clients.push(em.value)
-    }
-}
+
 function Read_Data(){
-    if(user_input.value == localStorage.getItem('clientPass')){
+    if(user_input.value == localStorage.getItem('pass')){
         window.alert('nice');
         window.open('mailto:apoel25nikolas@gmail.com?subject=Write your feedback below!&body=');
-    }else if(user_input.value != localStorage.getItem('clientPass') || user_input.value == null){
+    }else if(user_input.value != localStorage.getItem('pass') || user_input.value == null){
         window.alert('trash');
     }
 }
